caf3367ed56e10fc42080af8d1bc2604251b1227
aman.singh <aman.singh@helpshift.com>
2025-05-09 17:14:32 +0530

    update fastAPI config

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Replace with Vercel domain in production
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
